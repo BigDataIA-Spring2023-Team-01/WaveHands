1774a1ff319a9cc2a149392ce5a7662525292768
CY2404 <114796225+CY2404@users.noreply.github.com>
2023-04-19 18:47:11 -0400

    update word book page

--- streamlit/pages/2_WordBook.py
+++ streamlit/pages/2_WordBook.py
@@ -2,7 +2,7 @@
 # Importing Libraries 
 import streamlit as st 
 import pandas as pd
-import nltk #for natural language processing (if we want to add topics to read as well)
+import nltk
 
 
 
@@ -10,3 +10,37 @@ import nltk #for natural language processing (if we want to add topics to read a
 
 #-------------------------------------------------------------------------------------------------------------------------------
 #Fundamental template
+
+#load different sign languages
+def load_data(language):
+    if language == "American Sign Language":
+        data = pd.read_csv("english_data.csv")
+    elif language == "Indian Sign Language":
+        data = pd.read_csv("french_data.csv")
+    elif language == "Bengali Sign Language":
+        data = pd.read_csv("bengali_data.csv")        
+    #Add other languages as required and the csv of the respective files 
+
+    return data
+
+def word_book():
+    st.title("Word-Book")
+
+    language = st.selectbox("Select Sign Language Module", ("American Sign Language", "Indian Sign Language","Bengali Sign Language")) #Can Add other languages as required
+    word = st.text_input("Enter a Word")
+
+    if st.button("Search"):
+        data = load_data(language)
+        result = data.loc[data['word'] == word]
+
+        if not result.empty:
+            st.write("Definition: ", result.iloc[0]['definition'])
+            
+        else:
+            st.write("No results found for the word '{}' in {}".format(word, language))
+
+def main():
+    word_book()
+
+if __name__ == "__main__":
+    main()
