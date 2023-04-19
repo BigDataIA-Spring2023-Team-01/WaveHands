2b3f09e1731b7b2632e31f86dd863863aada6832
CY2404 <114796225+CY2404@users.noreply.github.com>
2023-04-19 19:22:27 -0400

    updated subscription info

--- streamlit/pages/1_Login.py
+++ streamlit/pages/1_Login.py
@@ -13,7 +13,7 @@ USER_CREDENTIALS = {
     "user3": "password3"
 }
 
-PLANS = {"Free Tier", "Silver Tier", "Gold Tier", "Platinum Tier"}
+
 
 
 
@@ -43,10 +43,27 @@ def signup():
     firstname = st.text_input("First Name")
     lastname = st.text_input("Last Name")
     email = st.text_input("Email")
+    st.write("Choose your subscription plan:")
+    subscription_plans = {"Free": "$0/month", "Basic": "$10/month", "Premium": "$25/month"}
+    selected_plan = st.radio("", list(subscription_plans.keys()))
+
+    ## Can add this too for payment information 
+    # if selected_plan != "Free":
+    #     st.write("Enter your payment information:")
+    #     card_number = st.text_input("Card Number")
+    #     expiration_date = st.text_input("Expiration Date")
+    #     cvv = st.text_input("CVV")
+    #     confirm = st.checkbox("I confirm my subscription to {} plan for {}.".format(selected_plan, subscription_plans[selected_plan]))
+    #     if confirm:
+    #         st.success("Subscription confirmed")
+    #     else:
+    #         st.warning("Please confirm your subscription to proceed")
+
+    
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     confirm = st.text_input("Confirm Password", type="password")
-
+    
     if st.button("Sign Up"):
         if username in USER_CREDENTIALS:
             st.error("Username already taken")
@@ -58,7 +75,7 @@ def signup():
 #-------------------------------------------------------------------------------------------------------------------
 #main function
 def main():
-    st.sidebar.title("Navigation")
+    st.sidebar.title("Registration")
     page = st.sidebar.radio("Go to", ("Login", "Sign Up"))
 
     if page == "Login":
