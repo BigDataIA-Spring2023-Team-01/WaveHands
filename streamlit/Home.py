d843428e8ce20f461db634bbb4a57961e0613fd1
CY2404 <114796225+CY2404@users.noreply.github.com>
2023-04-19 17:02:54 -0400

    streamlit pages

---
+++ streamlit/Home.py
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------------------------------------------------------
+# Importing Libraries 
+import streamlit as st 
+import time
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
+#Fundamental template
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+# st.sidebar.success("Select a demo above.")
+
