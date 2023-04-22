cd62916eae709ed91aa805096e7340ed22a2150b
shaminchokshi <62183907+shaminchokshi@users.noreply.github.com>
2023-04-22 01:38:05 -0400

    Commit with Handspeak and signit template

--- streamlit/pages/3_SignIt.py
+++ streamlit/pages/3_SignIt.py
@@ -9,3 +9,23 @@ import time
 
 #-------------------------------------------------------------------------------------------------------------------------------
 #Fundamental template
+import streamlit as st
+import os
+
+
+def main():
+    st.title("SignIt")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("## Upload an audio file")
+        uploaded_file = st.file_uploader("Choose a file", type=["mp3", "m4a"])
+    
+    with col2:
+        st.write("## Paste a YouTube Link")
+        youtubelink = st.text_input("Youtube Link", "")
+     
+    st.write("## Signed It ✅")
+    st.video("https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4")
+   
+if __name__ == "__main__":
+    main()
--- streamlit/pages/4_HandSpeak.py
+++ streamlit/pages/4_HandSpeak.py
@@ -2,7 +2,7 @@
 # Importing Libraries 
 import streamlit as st 
 import time
-
+import cv2
 
 
 
@@ -10,3 +10,18 @@ import time
 
 #-------------------------------------------------------------------------------------------------------------------------------
 #Fundamental template
+
+def main():
+    st.title("HandSpeak")
+    run = st.checkbox('Start Recording')
+    FRAME_WINDOW = st.image([])
+    video_capture = cv2.VideoCapture(0)
+
+    while run:
+        ret, frame = video_capture.read()
+        FRAME_WINDOW.image(frame)
+    
+    video_capture.release()
+
+if __name__ == "__main__":
+    main()
