ff2812c5eb36558f9a3bbf45b20fae56951bf172
Anandita <deb.ana@northeastern.edu>
2023-04-25 01:03:54 -0400

    Home_streamlit

---
+++ streamlit/Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+# #-------------------------------------------------------------------------------------------------------------------------------
+#Fundamental template
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# WaveHands 👋")
+st.write("Bridging the gap between deaf and hearing world")
+
+
+
+
+
+
