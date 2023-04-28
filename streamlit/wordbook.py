4d61396c179a571ce8c44373b0ca51c526a5811d
Anandita <deb.ana@northeastern.edu>
2023-04-28 13:18:07 -0400

    wordbook

--- streamlit/wordbook.py
+++ streamlit/wordbook.py
@@ -1,81 +1,114 @@
 import streamlit as st
 import os
-import random
-import sqlite3
-import os
-# os.chdir('./WaveHands/')
 
-# def callback():
-#     st.session_state.button_click = True
-# if 'button_click' not in st.session_state:
-#     st.session_state.button_click = False
+# Set the path for the selected sign language and set number
+selected_set = None
+selected_lang = None
+set_path = None
 
-st.title("Sign Language Videos")
-options = ["American Sign Language", "Indian Sign Language", "Spanish Sign Language"]
+# Create a dictionary to store user inputs
+user_input = {}
+vid_displayed_order={}
 
-selected_option = st.selectbox("Select a sign language", options)
-options = ["Learn the alphabet", "Easy Level 1","Easy Level 2","Medium Level 1","Medium Level 2","Medium Level 3","Difficult Level 1","Difficult Level 2","Difficult Level 3"]
-bank = st.selectbox("Select level for questions", options)
+def extract_names_from_filename (s):
+    fname = s.split("/")[3]
+    char = fname.split("_")[2]
+    return char
 
-if bank == "Learn the alphabet":
-    plan_number = 1
-elif bank == "Easy Level 1":
-    plan_number = 2
-elif bank == "Easy Level 2":
-    plan_number = 3
-elif bank == "Medium Level 1":
-    plan_number = 4
-elif bank == "Medium Level 2":
-    plan_number = 5
-elif bank == "Medium Level 3":
-    plan_number = 6
-elif bank == "Difficult Level 1":
-    plan_number = 7
-elif bank == "Difficult Level 2":
-    plan_number = 8
-elif bank == "Difficult Level 3":
-    plan_number = 9
-
-# answer = "None"
-asl_plan_paths = f"./ASL_Wordbook/set{plan_number}_videos"
-
-
-#---------------------- GOOD CODE
-def submit(answer):
-    global word
-    input_text = st.session_state.input_text
-    if input_text.lower() == answer.lower():
-        st.success('Correct!')
-        word = videos()
-    else:
-        st.error('Please try again.')
-    st.session_state.input_text = ""  # clear text input box
-
-
-def videos():
-    video_files = []
-    # populate list of video files
-    for filename in os.listdir(asl_plan_paths):
-        if filename.endswith('.mp4'):
-            filepath = os.path.join(asl_plan_paths, filename)
-            video_files.append(filepath)
-    random.shuffle(video_files)
-    # extract filename "word" from video path
-    filename = os.path.basename(video_files[0])
-    print(video_files[0])
-    answer = filename.split('_')[2]
-    print(filename)
-    print(answer.lower())
-    st.video(video_files[0])
+# Define a function to display the videos and text inputs
+def display_videos(set_path):
+    videos = os.listdir(set_path)
+    videos = [vid for vid in videos if vid.endswith(".mp4")]
+    for i in range(0, len(videos), 2):
+        col1, col2 = st.columns(2)
+        video1 = os.path.join(set_path, videos[i])
+        print(video1)
+        col1.video(video1)
+        vid_displayed_order[i]=extract_names_from_filename(video1)
+        col1_textbox = col1.text_input(label="Enter the word for this image", key=i)
+        user_input[i] = col1_textbox
+        
+        if i+1 < len(videos):
+            video2 = os.path.join(set_path, videos[i+1])
+            print(video2)
+            col2.video(video2)
+            vid_displayed_order[i+1]=extract_names_from_filename(video2)
+            col2_textbox = col2.text_input(label="Enter the word for this image", key=i+1)
+            user_input[i+1] = col2_textbox
     
-    return answer
+    print("vid_displayed_order({}): {}".format(type(vid_displayed_order),vid_displayed_order))
 
-word = videos()
-submit = st.button("Go")
-if submit:
-    submit(word)
+
+
+# Define a function to check user input against the image filenames
+def check_answers(set_path, user_input):
+    print("user_input({}): {}".format(type(user_input),user_input))
+    results = []
+    for (set_i, set_n), (input_i, input_n) in zip(vid_displayed_order.items(), user_input.items()):
+        results.append(set_n == input_n.lower())
+    return results
+
+# Define the Streamlit app
+def app():
+    global selected_set, selected_lang, set_path
     
-    input_text = st.text_input('Enter the word in the video:', key="input_text")
-    st.session_state.input_text = input_text  # store input text in session state
-    if input_text != "":
-        print(input_text)
+    st.title("ASL Wordbook")
+    
+    # Select the sign language
+    selected_lang = st.selectbox("Select sign language", ["ASL"])
+    difficulty = ["Learn the alphabet", "Easy Level 1","Easy Level 2","Medium Level 1","Medium Level 2","Medium Level 3","Difficult Level 1","Difficult Level 2","Difficult Level 3"]
+    bank = st.selectbox("Select level for questions", difficulty)
+
+    if bank == "Learn the alphabet":
+        plan_number = "set1"
+    elif bank == "Easy Level 1":
+        plan_number = "set2"
+    elif bank == "Easy Level 2":
+        plan_number = "set3"
+    elif bank == "Medium Level 1":
+        plan_number = "set4"
+    elif bank == "Medium Level 2":
+        plan_number = "set5"
+    elif bank == "Medium Level 3":
+        plan_number = "set6"
+    elif bank == "Difficult Level 1":
+        plan_number = "set7"
+    elif bank == "Difficult Level 2":
+        plan_number = "set8"
+    elif bank == "Difficult Level 3":
+        plan_number = "set9"
+    # Select the set number
+    selected_set = bank
+    
+    # Set the path for the selected sign language and set number
+    set_path = os.path.join("streamlit/ASL_Wordbook", f"{plan_number}_videos")
+    
+    # Check if the set has been changed
+    if "last_set" not in st.session_state:
+        st.session_state.last_set = None
+
+    if st.session_state.last_set != selected_set:
+        user_input.clear()
+    
+    # Display the videos and text inputs
+    display_videos(set_path)
+    
+    # Check the answers
+    if st.button("Check Answers"):
+        results = check_answers(set_path, user_input)
+        for i in range(len(results)):
+            if results[i]:
+                st.write(f"Image {i+1} is CORRECT!")
+            else:
+                st.write(f"Image {i+1} is INCORRECT.")
+    
+    # Save the current set as the last set
+    st.session_state.last_set = selected_set
+
+# Run the app
+if __name__ == "__main__":
+    app()
+
+
+
+
